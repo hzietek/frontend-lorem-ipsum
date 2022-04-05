05e1c4824d24e50e7e8b080fb57ac021cdc6d570
Hubert Ziętek <hubazietek3@gmail.com>
2022-04-05 18:00:28 +0200

    Update hero.js

--- src/js/hero.js
+++ src/js/hero.js
@@ -4,21 +4,23 @@ const leftArrow = document.querySelector(".left-arrow");
 const rightArrow = document.querySelector(".right-arrow");
 
 leftArrow.addEventListener("click", () => {
-  let scroll = window.innerWidth;
+  let scroll = window.innerWidth; // why not const
   hero.scrollLeft += -scroll;
-  let bulletNumber = Math.round(
+  let bulletNumber = Math.round( 
     (hero.scrollLeft - window.innerWidth) / window.innerWidth
-  );
+  ); //why not const
   if (bulletNumber >= 0 && bulletNumber <= 3) {
-    bullet.forEach((el) => (el.style.backgroundColor = "#eee"));
+    bullet.forEach((el) => (el.style.backgroundColor = "#eee")); // again, dont use style, add class and then change style in css
     bullet[
       Math.round((hero.scrollLeft - window.innerWidth) / window.innerWidth)
-    ].style.backgroundColor = "#DEAC28";
-  } else console.log(bulletNumber);
+    ].style.backgroundColor = "#DEAC28"; // again, dont use style, add class and then change style in css
+  } else console.log(bulletNumber); // why console.log
 });
 
+// same stuff for rest of lines (classes, const, console logs)
+
 rightArrow.addEventListener("click", () => {
-  let scroll = window.innerWidth;
+  let scroll = window.innerWidth; 
   hero.scrollLeft += scroll;
   let bulletNumber = Math.round(
     (hero.scrollLeft + window.innerWidth) / window.innerWidth
