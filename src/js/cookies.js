258b0374eec2a15dda75660857562590adcad1dd
Hubert Ziętek <hubazietek3@gmail.com>
2022-04-05 17:57:31 +0200

    Update cookies.js

--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,6 +1,7 @@
 const cookiesButton = document.querySelector(".cookies-button");
 
 cookiesButton.addEventListener("click", () => {
-  const wrapper = document.querySelector(".cookies-wrapper");
-  wrapper.style.display = "none";
+  const wrapper = document.querySelector(".cookies-wrapper"); // move it to line 2
+  wrapper.style.display = "none"; // don't use style in JS, toggle class in JS then add styles in css, e.g addClass("hidden") and then in
+  // css on class hidden add display: none
 });
